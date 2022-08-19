8ebd5ff98e19129db844a4d9f1e9b379a8997047
Amal Vitorino <69655550+amalvitorino@users.noreply.github.com>
2022-08-19 00:14:43 -0700

    pequenos ajustes

--- crud_procedural/index.php
+++ crud_procedural/index.php
@@ -26,31 +26,45 @@ include_once 'includes/mesnagem.php';
                 <?php
                 $sql = "SELECT * FROM `alunos`";
                 $resultado = mysqli_query($conexao, $sql);
-                while ($aluno = mysqli_fetch_array($resultado)) : ?>
-                    <tr>
-                        <td><?php echo $aluno['nome']; ?></td>
-                        <td><?php echo $aluno['apelido']; ?></td>
-                        <td><?php echo $aluno['data_nascimento']; ?></td>
-                        <td><?php echo $aluno['email']; ?></td>
-                        <td><a class="btn-floating orange" href="editar.php?id=<?php echo $aluno['id'] ?>"><i class="material-icons">edit</i></a></td>
-                        <td><a class="btn-floating red modal-trigger" href="#modal1<?php echo $aluno['id'] ?>"><i class="material-icons">delete</i></a></td>
 
-                        <!-- Modal Structure -->
-                        <div id="modal1<?php echo $aluno['id'] ?>" class="modal">
-                            <div class="modal-content">
-                                <h4>Opah!</h4>
-                                <p>Tem certeza que deseja excluir esse registo?</p>
+                if (mysqli_num_rows($resultado) > 0) :
+                    while ($aluno = mysqli_fetch_array($resultado)) : ?>
+                        <tr>
+                            <td><?php echo $aluno['nome']; ?></td>
+                            <td><?php echo $aluno['apelido']; ?></td>
+                            <td><?php echo $aluno['data_nascimento']; ?></td>
+                            <td><?php echo $aluno['email']; ?></td>
+                            <td><a class="btn-floating orange" href="editar.php?id=<?php echo $aluno['id'] ?>"><i class="material-icons">edit</i></a></td>
+                            <td><a class="btn-floating red modal-trigger" href="#modal1<?php echo $aluno['id'] ?>"><i class="material-icons">delete</i></a></td>
+
+                            <!-- Modal Structure -->
+                            <div id="modal1<?php echo $aluno['id'] ?>" class="modal">
+                                <div class="modal-content">
+                                    <h4>Opah!</h4>
+                                    <p>Tem certeza que deseja excluir esse registo?</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <form action="php_action/delete.php" method="post">
+                                        <input type="hidden" name="id" value="<?php echo $aluno['id'] ?>">
+                                        <button type="submit" name="btn_deletar" class="btn red">Sim</button>
+                                        <a href="#!" class="modal-close waves-effect waves-green btn">Não</a>
+                                    </form>
+                                </div>
                             </div>
-                            <div class="modal-footer">
-                                <form action="php_action/delete.php" method="post">
-                                    <input type="hidden" name="id" value="<?php echo $aluno['id'] ?>">
-                                    <button type="submit" name="btn_deletar" class="btn red">Sim</button>
-                                    <a href="#!" class="modal-close waves-effect waves-green btn">Não</a>
-                                </form>
-                            </div>
-                        </div>
+                        </tr>
+                    <?php endwhile;
+                else :
+                    ?>
+                    <tr>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
                     </tr>
-                <?php endwhile; ?>
+                <?php
+                endif;
+                ?>
             </tbody>
         </table><br>
         <a href="adicionar.php" class="btn">Adicionar Aluno</a>
